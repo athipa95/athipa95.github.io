1e96cd557721033e14ed028efee8fcf7ffd20b26
athipa95 <athipt@arizona.edu>
2024-12-30 17:03:31 -0700

    Removed (Commented out) the Contact me stuff here from index.html, style.css, script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -305,33 +305,33 @@ $(".project").hover(
 	}
 );
 
-/* -------------------- ----------------------- -------------------- */
-/* -------------------- Contact Form Submission -------------------- */
-/* -------------------- ----------------------- -------------------- */
-$("#contentPage3").submit(function (e) {
-	e.preventDefault();
-	$.ajax({
-		url: "https://formspree.io/f/mjkkeweq",
-		method: "POST",
-		data: { name: $("#userName").val(), _replyto: $("#userEmail").val(), message: $("#userMessage").val() },
-		dataType: "json"
-	});
+// /* -------------------- ----------------------- -------------------- */
+// /* -------------------- Contact Form Submission -------------------- */
+// /* -------------------- ----------------------- -------------------- */
+// $("#contentPage3").submit(function (e) {
+// 	e.preventDefault();
+// 	$.ajax({
+// 		url: "https://formspree.io/f/mjkkeweq",
+// 		method: "POST",
+// 		data: { name: $("#userName").val(), _replyto: $("#userEmail").val(), message: $("#userMessage").val() },
+// 		dataType: "json"
+// 	});
 
-	setTimeout(function () {
-		clearForm();
-	}, 1000);
-});
+// 	setTimeout(function () {
+// 		clearForm();
+// 	}, 1000);
+// });
 
-function clearForm() {
-	var elems = document.getElementsByTagName("input");
-	var l = elems.length - 1;
-	for (var i = 0; i < l; ++i) {
-		elems[i].value = "";
-	}
-	$("textarea").val("");
+// function clearForm() {
+// 	var elems = document.getElementsByTagName("input");
+// 	var l = elems.length - 1;
+// 	for (var i = 0; i < l; ++i) {
+// 		elems[i].value = "";
+// 	}
+// 	$("textarea").val("");
 
-	alert("Contact request sent.");
-}
+// 	alert("Contact request sent.");
+// }
 
 /* -------------------- --------------- -------------------- */
 /* -------------------- Google Tracking -------------------- */
